585a4c82534b7af6ff5ed63bebeceae5e3c9559d
吴凡 <1971113508@qq.com>
2021-08-06 16:56:08 +0800

    init mvtec

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -103,7 +103,7 @@ class PMASDataset(Dataset):
         self.phase=phase
         self.transform_img=T.Compose([T.Resize((imsize,imsize), Image.ANTIALIAS),T.ToTensor(),
                                       T.Normalize(mean=mean,std=std)])
-        self.transform_mask=T.Compose([T.Resize((imsize,imsize), Image.ANTIALIAS),T.ToTensor()])
+        self.transform_mask=T.Compose([T.Resize((imsize,imsize), Image.NEAREST),T.ToTensor()])
         self.generate_presudo_mask=CoCoPseudoMask("/data/coco/images/val","/data/coco/annotations/instances_val2017.json")
         self.all_imgs = self.load_dataset_folder()
 
@@ -139,3 +139,80 @@ class PMASDataset(Dataset):
                                     and int(f.split('.')[0])%self.sparse==0 ])
             imgs.extend(img_fpath_list)
         return list(imgs)
+
+class MVTecDataset(Dataset):
+    def __init__(self,dataset_path='/data/VAD/mvtec/capsule',imsize=256, phase="train",
+                 mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
+        self.dataset_path = dataset_path
+        self.imsize=imsize
+        self.phase=phase
+        self.transform_img=T.Compose([T.Resize((imsize,imsize), Image.ANTIALIAS),T.ToTensor(),
+                                      T.Normalize(mean=mean,std=std)])
+        self.transform_mask=T.Compose([T.Resize((imsize,imsize), Image.NEAREST),T.ToTensor()])
+        self.generate_presudo_mask=CoCoPseudoMask("/data/coco/images/val","/data/coco/annotations/instances_val2017.json")
+        self.x,self.y,self.mask = self.load_dataset_folder()
+
+    def __getitem__(self, idx):
+        if self.phase=="train":
+            img = cv2.imread(self.x[idx])
+            img,mask,label = self.generate_presudo_mask(img)
+            img = cv2.GaussianBlur(img, ksize=(3,3), sigmaX=0, sigmaY=0)
+            img = Image.fromarray(cv2.cvtColor(img,cv2.COLOR_BGR2RGB)).convert('RGB')
+            img = self.transform_img(img)
+            mask = mask[:,:,0]
+            mask = self.transform_mask(Image.fromarray(mask))
+            return img,mask,label
+        else:
+            img = cv2.imread(self.x[idx])
+            if self.mask[idx] is None:
+                mask = np.zeros_like(img)[:,:,0]
+                mask = Image.fromarray(mask)
+            else:
+                mask = Image.open(self.mask[idx]).convert('L')
+            mask = self.transform_mask(mask)
+            
+            img = cv2.GaussianBlur(img, ksize=(3,3), sigmaX=0, sigmaY=0)
+            img = Image.fromarray(cv2.cvtColor(img,cv2.COLOR_BGR2RGB)).convert('RGB')
+            img = self.transform_img(img)
+            label = self.y[idx]
+            label = torch.tensor([label]).float()
+
+            return img,mask,label
+
+    def __len__(self):
+        return len(self.x)
+
+    def load_dataset_folder(self):
+        x, y, mask = [], [], []
+
+        img_dir = os.path.join(self.dataset_path, self.phase)
+        gt_dir = os.path.join(self.dataset_path, 'ground_truth')
+
+        img_types = sorted(os.listdir(img_dir))
+        for img_type in img_types:
+
+            # load images
+            img_type_dir = os.path.join(img_dir, img_type)
+            if not os.path.isdir(img_type_dir):
+                continue
+            img_fpath_list = sorted([os.path.join(img_type_dir, f)
+                                    for f in os.listdir(img_type_dir)
+                                    if f.endswith(('.jpg','.png'))])
+            x.extend(img_fpath_list)
+
+            # load gt labels
+            if img_type == 'good':
+                y.extend([0] * len(img_fpath_list))
+                mask.extend([None] * len(img_fpath_list))
+            else:
+                y.extend([1] * len(img_fpath_list))
+                gt_type_dir = os.path.join(gt_dir, img_type)
+                img_fname_list = [os.path.splitext(os.path.basename(f))[0] for f in img_fpath_list]
+                gt_fpath_list = [os.path.join(gt_type_dir, img_fname + '_mask.png')
+                                for img_fname in img_fname_list]
+                mask.extend(gt_fpath_list)
+
+        assert len(x) == len(y), 'number of x and y should be same'
+        return list(x), list(y), list(mask)
+
+    
--- eval.py
+++ eval.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from multiprocessing import Pool
 from model.pmas import PMAS
 from sklearn.metrics import roc_auc_score
-from dataset.dataset import PMASDataset
+from dataset.dataset import MVTecDataset
 from torch.utils.data import DataLoader
 
 
@@ -24,46 +24,49 @@ def normalize_psnr(psnr_list,inverse=False):
             result.append(1-temp)
     return result
 
-def evaluation(model,gt_path,dataset_path,batch_size,im_size,device,slide_avg=0.25):
-    ground_th=np.load(gt_path)
-    if not gt_path.split('.')[0].endswith('shanghai'):
-        ground_th=ground_th[0]
-        
-    dataset = PMASDataset(dataset_path=dataset_path,imsize=im_size, sparse=1, phase="test")
-    loader = DataLoader(dataset,batch_size=batch_size,shuffle=False,num_workers=16)
+def evaluation(model,dataset_path,batch_size,im_size,device,slide_avg=0.25):
+
+    dataset = MVTecDataset(dataset_path=dataset_path,imsize=im_size, phase="test")
+    loader = DataLoader(dataset,batch_size=batch_size,shuffle=False,num_workers=1)
+    print("=> total num: {}".format(len(dataset)))
     score_list=[]
+    gt_list=[]
+
+    map_list=[]
+    map_gt=[]
     with torch.no_grad():
-        for i, (img) in tqdm(enumerate(loader),"processing evaluation",total=None,leave=False,unit="img"):
+        for i, (img,mask,label) in tqdm(enumerate(loader),"=> processing evaluation",total=None):
             img=img.to(args.device)
             pred_mask,pred_label=model(img)
-            pred_label=pred_label[:,0]
-            score_list+=pred_label.cpu().detach().numpy().tolist()
 
-    auc=roc_auc_score(ground_th[0:len(score_list)],score_list)
-    return auc
+            score_list+=pred_label.cpu().detach().flatten().tolist()
+            gt_list+=label.detach().flatten().tolist()
+
+            map_list+=pred_mask.cpu().detach().flatten().tolist()
+            map_gt+=mask.detach().flatten().tolist()
+    print("=> inference done, calculating AUC...")
+    img_auc=roc_auc_score(gt_list,score_list)
+    pix_auc=roc_auc_score(map_gt,map_list)
+    return img_auc,pix_auc
     
 def main(args):
-    Dict={"SHTech":"frame_labels_shanghai.npy",
-          "Ped2":"frame_labels_ped2.npy",
-          "Avenue":"frame_labels_avenue.npy"}
-    gt_path=os.path.join(os.path.dirname(args.dataset_path),Dict[os.path.basename(args.dataset_path)])
-    
     model=PMAS().to(args.device)
     model.load_state_dict(torch.load(args.weights)[0])
     model.eval()
-    auc=evaluation(model,gt_path,args.dataset_path,args.batch_size,args.imsize,args.device)
-    print("\n=> evaluation auc: {:.3f}.".format(auc))
+    img_auc,pix_auc=evaluation(model,args.dataset_path,args.batch_size,args.imsize,args.device)
+    print("=> evaluation image auc: {:.3f}, pixel auc: {:.3f}.\n".format(img_auc,pix_auc))
     
 if __name__=="__main__":
     torch.multiprocessing.set_start_method('spawn')
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset_path', type=str, default='/data/VAD/SHTech', help='dataset path')
+    parser.add_argument('--dataset_path', type=str, default='/data/VAD/mvtec/capsule', help='dataset path')
     parser.add_argument('--batch_size', type=int, default='2')
     parser.add_argument('--weights', type=str, default='result/checkpoint/best.pt', help='model.pt path(s)')
     parser.add_argument('--imsize', type=int, default=256, help='inference size (pixels)')
     parser.add_argument('--device', default='cuda', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
 
     args = parser.parse_args()
+    print()
     print(args)
     main(args)
     
