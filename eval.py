9ea02c1fc84d7729261aab484aa8f863ac2cd9f3
吴凡 <1971113508@qq.com>
2021-08-09 17:29:37 +0800

    add ruc plot

--- eval.py
+++ eval.py
@@ -2,12 +2,9 @@ import os,sys,natsort,argparse,time,subprocess,cv2
 import torch,torchvision,einops
 import numpy as np
 from tqdm import tqdm
-from PIL import Image,ImageFilter
-from torchvision import transforms as T
-from datetime import datetime
-from multiprocessing import Pool
 from model.pmas import PMAS
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_auc_score,roc_curve
+import matplotlib.pyplot as plt
 from dataset.dataset import MVTecDataset
 from torch.utils.data import DataLoader
 
@@ -25,42 +22,64 @@ def normalize_psnr(psnr_list,inverse=False):
     return result
 
 def evaluation(model,dataset_path,batch_size,im_size,device,slide_avg=0.25):
+    all=['cable', 'leather', 'transistor', 'hazelnut', 'pill', 'wood',
+            'toothbrush', 'bottle', 'zipper', 'carpet', 'screw', 
+            'tile', 'metal_nut', 'grid', 'capsule']
+    fig, ax = plt.subplots(1, 2, figsize=(20, 10))
+    fig_img_rocauc = ax[0]
+    fig_pixel_rocauc = ax[1]
+    AUC1,AUC2=[],[]
+    for class_name in all:
+        dataset = MVTecDataset(dataset_path=dataset_path,imsize=im_size, phase="test",class_name=class_name)
+        loader = DataLoader(dataset,batch_size=batch_size,shuffle=False,num_workers=1)
+        print("=> '{}' total num: {}".format(class_name,len(dataset)))
+        score_list=[]
+        gt_list=[]
 
-    dataset = MVTecDataset(dataset_path=dataset_path,imsize=im_size, phase="test")
-    loader = DataLoader(dataset,batch_size=batch_size,shuffle=False,num_workers=1)
-    print("=> total num: {}".format(len(dataset)))
-    score_list=[]
-    gt_list=[]
+        map_list=[]
+        map_gt=[]
+        with torch.no_grad():
+            for i, (img,mask,label) in tqdm(enumerate(loader),"=> processing evaluation"):
+                img=img.to(device)
+                pred_mask,pred_label=model(img)
 
-    map_list=[]
-    map_gt=[]
-    with torch.no_grad():
-        for i, (img,mask,label) in tqdm(enumerate(loader),"=> processing evaluation"):
-            img=img.to(device)
-            pred_mask,pred_label=model(img)
+                score_list+=pred_label.cpu().detach().flatten().tolist()
+                gt_list+=label.detach().flatten().tolist()
 
-            score_list+=pred_label.cpu().detach().flatten().tolist()
-            gt_list+=label.detach().flatten().tolist()
-
-            map_list+=pred_mask.cpu().detach().flatten().tolist()
-            map_gt+=mask.detach().flatten().tolist()
-    print("=> inference done, calculating AUC...")
-    img_auc=roc_auc_score(gt_list,score_list)
-    pix_auc=roc_auc_score(map_gt,map_list)
-    return img_auc,pix_auc
+                map_list+=pred_mask.cpu().detach().flatten().tolist()
+                map_gt+=mask.detach().flatten().tolist()
+        print("=> inference done, calculating AUC...")
+        fpr,tpr, _ = roc_curve(gt_list, score_list)
+        img_auc = roc_auc_score(gt_list, score_list)
+        AUC1.append(img_auc)
+        fig_img_rocauc.plot(fpr, tpr, label='%s ROCAUC: %.3f' % (class_name, img_auc))
+        fig_img_rocauc.legend(loc="lower right")
+        
+        fpr,tpr, _ = roc_curve(map_gt, map_list)
+        pix_auc = roc_auc_score(map_gt, map_list)
+        AUC2.append(pix_auc)
+        fig_pixel_rocauc.plot(fpr, tpr, label='%s ROCAUC: %.3f' % (class_name, pix_auc))
+        fig_pixel_rocauc.legend(loc="lower right")
+        print("=> '{}' image auc: {:.3f}, pixel auc: {:.3f}.\n".format(class_name,img_auc,pix_auc))
+        
+    fig_img_rocauc.title.set_text('Average image ROCAUC: %.3f' % np.array(AUC1).mean())
+    fig_pixel_rocauc.title.set_text('Average pixel ROCAUC: %.3f' % np.array(AUC2).mean())
+    fig.tight_layout()
+    fig.savefig('result/eval_roc_curve.jpg', dpi=100)
+    return np.array(AUC1).mean(),np.array(AUC2).mean()
     
 def main(args):
     model=PMAS().to(args.device)
     model.load_state_dict(torch.load(args.weights)[0])
     model.eval()
     img_auc,pix_auc=evaluation(model,args.dataset_path,args.batch_size,args.imsize,args.device)
-    print("=> evaluation image auc: {:.3f}, pixel auc: {:.3f}.\n".format(img_auc,pix_auc))
+    print("=> average image auc: {:.3f}, average pixel auc: {:.3f}.\n".format(img_auc,pix_auc))
     
 if __name__=="__main__":
     torch.multiprocessing.set_start_method('spawn')
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset_path', type=str, default='/data/VAD/mvtec', help='dataset path')
-    parser.add_argument('--batch_size', type=int, default='32')
+    parser.add_argument('--batch_size', type=int, default='40')
     parser.add_argument('--weights', type=str, default='result/checkpoint/best.pt', help='model.pt path(s)')
     parser.add_argument('--imsize', type=int, default=256, help='inference size (pixels)')
     parser.add_argument('--device', default='cuda', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
